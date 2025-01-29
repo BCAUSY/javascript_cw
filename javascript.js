3ba51836ea9e23faefcd29c9aaf4b70bf486edbc
bacusy <191461931+BCAUSY@users.noreply.github.com>
2025-01-29 19:05:48 +0100

    made the computer choice function

--- javascript.js
+++ javascript.js
@@ -1,2 +1,34 @@
-console.log("hello");
+
+/* const rock = 1;
+const paper = 2;
+const scissors = 3; */
+
+const choice = ["rock", "paper", "scissors"];
+/* console.log(choice);
+console.log(rock); */
+
+function getComputerChoice(){
+     return choice[(Math.floor(Math.random() * 3))];
+     };
+
+
+  
+console.log(choice);
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+
+
+
+
+
+
+
+
+
+
+
+
 
