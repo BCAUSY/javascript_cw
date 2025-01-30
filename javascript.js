65747eedb79b2bf95b7c865b927990afa48ca4ce
bacusy <191461931+BCAUSY@users.noreply.github.com>
2025-01-30 21:41:15 +0100

    basic css added js buttons defined, getHumanchoicefunction updated

--- javascript.js
+++ javascript.js
@@ -1,34 +1,54 @@
+const buttonRound = document.getElementById("buttonRound");
+const buttonGame = document.getElementById("buttonGame");
 
-/* const rock = 1;
-const paper = 2;
-const scissors = 3; */
+const buttonRock = document.getElementById("buttonRock");
+const buttonPaper = document.getElementById("buttonPaper");
+const buttonScissors = document.getElementById("buttonScissors");
 
-const choice = ["rock", "paper", "scissors"];
-/* console.log(choice);
-console.log(rock); */
+
+
+
+// PLAYER CHOICE BUTTONS
+
+function getHumanChoice(choice){
+     console.log(`Human chose ${choice}`);
+};
+buttonRock.addEventListener("click", () => {
+    getHumanChoice("ROCK");
+});
+
+buttonPaper.addEventListener("click", () => {
+     getHumanChoice("PAPER");
+})
+buttonScissors.addEventListener("click", () => {
+     getHumanChoice("Scissors");
+})
+
+
+
+
+
+const choice = ["ROCK", "PAPER", "SCISSORS"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
      return choice[(Math.floor(Math.random() * 3))];
-     };
+};
 
 
-  
-console.log(choice);
+console.log(`Computer chose ${getComputerChoice()}`);
+console.log(getHumanChoice())
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+/* // ROUND FUNCTION 
 
+function playRound(humanChoice, computerChoice){
+     console.log(humanChoice);
+}
 
+playRound(humanSelection, computerSelection);
 
+const humanSelection = getHumanChoice();
+const computerSelection + getComputerChoice();
 
-
-
-
-
-
-
-
-
-
+console.log(computerSelection +"\n" + humanSelection);) */
