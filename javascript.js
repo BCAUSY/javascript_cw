ae54692bf0e4d6a80908ed344e6cc3494e4af123
bacusy <191461931+BCAUSY@users.noreply.github.com>
2025-02-07 09:25:59 +0100

    ui and functionality fixed

--- javascript.js
+++ javascript.js
@@ -1,16 +1,13 @@
-const buttonContainer = document.getElementById("button-container");
-const buttonRound = document.getElementById("buttonRound");
 
-const scoreContainer = document.querySelector("#score-container");
 const computerScoreText = document.getElementById("computerScoreText");
 const humanScoreText = document.getElementById("humanScoreText");
-
+const playGame = document.getElementById("playGame")
 const tools = document.getElementById("tools");
 const buttonRock = document.getElementById("buttonRock");
 const buttonPaper = document.getElementById("buttonPaper");
 const buttonScissors = document.getElementById("buttonScissors");
-
-const text = document.getElementById("text");
+let humanLast = document.getElementById("lastHuman");
+let compLast = document.getElementById("lastComp");
 const choice = ["ROCK", "PAPER", "SCISSORS"];
 
 let humanScore = 0;
@@ -18,24 +15,11 @@ let computerScore = 0;
 let humanSelection = "";
 let computerSelection = "";
 
-const menu = [
-  {
-    name: "main menu",
-    "button text": ["ROUND", "GAME"],
-    "button functions": [playRound, playGame],
-    text: "START A NEW GAME",
-  },
-  {
-    name: "game menu",
-    "button text": ["RESTART", "RESTART"],
-    "button functions": [restart, restart],
-    text: "FIGHT!",
-  },
-];
+
 // PLAYER CHOICE BUTTONS
-function getHumanChoice(choice) {
-  humanSelection = choice;
-  console.log(`Human chose: ${choice}`);
+function getHumanChoice(hchoice) {
+  humanSelection = hchoice;
+  console.log(`Human chose: ${humanSelection}`);
   return humanSelection;
 }
 
@@ -43,7 +27,7 @@ function getHumanChoice(choice) {
 buttonRock.addEventListener("click", () => {
   getHumanChoice("ROCK");
   getComputerChoice();
-  triggerGame();
+  playRound();
 });
 
 buttonPaper.addEventListener("click", () => {
@@ -67,50 +51,41 @@ function getComputerChoice() {
 computerSelection = getComputerChoice();
 
 /// layout shift
-buttonRound.onclick = toGameMenu;
-
 function restart() {
-  text.innerText = menu[0].text;
-
-  tools.style.display = "grid";
-  scoreContainer.style.display = "none";
-  buttonContainer.style.justifyContent = "space-around";
-  buttonRound.onclick = toGameMenu;
-  buttonRound.innerText = "PLAY ROUND";
   humanScore = 0;
   computerScore = 0;
   humanScoreText.innerText = "0";
   computerScoreText.innerText = "0";
 }
-
-function toGameMenu(menu) {
-  buttonContainer.style.justifyContent = "flex-end";
-  tools.style.display = "grid";
-  scoreContainer.style.display = "flex";
-  text.innerText = "FIGHT!";
-  buttonRound.innerText = "RUN";
-  buttonRound.onclick = restart;
-}
+playGame.addEventListener("click", () => {
+  console.log("ggg");
+  playGame.innerText = "RUN";
+  playGame = restart();
+})
 
 console.log(humanSelection);
 console.log(computerSelection);
 
 function playRound() {
   if (humanSelection === computerSelection) {
+    humanLast.innerHTML = humanSelection;
+    compLast.innerHTML = computerSelection;
     console.log("ITS A TIE");
-    text.innerHTML = `computer <strong>${computerSelection}</strong> <br> you chose <strong>${humanSelection}</strong>`;
   } else if (
     (humanSelection === "ROCK" && computerSelection === "SCISSORS") ||
     (humanSelection === "SCISSORS" && computerSelection === "PAPER") ||
     (humanSelection === "PAPER" && computerSelection === "ROCK")
   ) {
     console.log("YOU WIN");
-    text.innerHTML = `computer <strong>${computerSelection}</strong> <br> you chose <strong>${humanSelection}</strong>`;
+    humanLast.innerHTML = humanSelection;
+    compLast.innerHTML = computerSelection;
+
     humanScore += 1;
     humanScoreText.innerText = humanScore;
     console.log(humanScore);
   } else {
-    text.innerHTML = `computer <strong>${computerSelection}</strong> <br> you chose <strong>${humanSelection}</strong>`;
+    humanLast.innerHTML = humanSelection;
+    compLast.innerHTML = computerSelection;
     computerScore += 1;
     computerScoreText.innerText = computerScore;
     console.log("COMPUTER WINS");
@@ -128,15 +103,4 @@ function triggerGame() {
   playRound();
 }
 
-function playGame() {
-  toGameMenu();
-  console.log("elo");
-}
 
-// ROUND FUNCTION
-
-/* function playRound(humanChoice, computerChoice){ */
-
-/* playRound(humanSelection, computerSelection); */
-
-/* console.log(computerSelection +"\n" + humanSelection);) */
